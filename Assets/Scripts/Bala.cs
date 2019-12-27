4e53ea9c8ef809c2d45c9b64df365b0bf7b94b88
katleen barros <barroskatleen@gmail.com>
2019-12-26 21:54:06 -0300

    Nave, pedra e lêiser

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public int speed = 6;
+    public void Start()
+    {
+        Rigidbody2D r2d = GetComponent<Rigidbody2D>();
+
+        r2d.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    public void  OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject Bala;
+    private void Update()
+    {
+        transform.Translate((Input.GetAxis("Vertical") * transform.up * 0.05f) + (Input.GetAxis("Horizontal") * transform.right * 0.05f));
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonUp(0))
+        {
+            Instantiate(Bala, transform.position, Quaternion.identity);
+        }
+    }
+}
